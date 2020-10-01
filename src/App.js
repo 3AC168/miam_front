54f66c0951c802d699dc034397dde6367472aafa
3AC168 <arianecheng@yahoo.com>
2020-10-01 23:23:30 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
           {/*<IngredientsAdd />*/}
           
           {/*<Utilisateur/>*/}          
-          {/*<Ingredients/>*/}
+          <Ingredients/>
       </div>
     </div>
 
